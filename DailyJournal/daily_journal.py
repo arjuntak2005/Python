75be5da2462b6c62d3f3c5a9e9e96603104b95c0
Arjun Tak <157879613+arjuntak2005@users.noreply.github.com>
2026-06-29 08:21:50 +0000

    Daily Journal v2

--- DailyJournal/daily_journal.py
+++ DailyJournal/daily_journal.py
@@ -1,16 +1,65 @@
-# 26-06-26 - My first python project.
-
-# Daily Journal: version 1:-
-path = "daily_journal.txt"
+# 27-06-26 - Daily Journal: version 2:-
+path = "DailyJournal/daily_journal.txt"
 from datetime import date
 todays_date = date.today().strftime("%d/%m/%Y")
-#date = input("Enter todays date: ")
-work_done = input("Enter Work Done: ")
-thoughts_feelings = input("Enter Thoughts/Feelings: ")
-learnings = input("Enter Learnings: ")
 
-with open(path, 'a') as journal:
-    journal.write(f"\n\n----#---- {todays_date} ----#----")
-    journal.write(
-        f"\nWork Done: {work_done}\nThoughts/Feelings: {thoughts_feelings}\nLearnings: {learnings}")
-    journal.write("\n-- ---- ---- ---- ---- ---- --")
+
+def Mode_Input():
+    try:
+        return int(input("\nEnter: '1'-Write Mode | '2' -Read Mode | '3' -Close   -> "))
+    except ValueError:
+        print("\nYou are supposed to enter one of the number mentioned above!")
+        return None
+
+
+def Mode_Selection():
+    while True:
+        mode_input = Mode_Input()
+        if mode_input == 1:
+            print("You are in Write Mode")
+            Write_Mode()
+        elif mode_input == 2:
+            print("You are in Read Mode")
+            Read_Mode()
+        elif mode_input == 3:
+            print("Closed..")
+            break
+        else:
+            print("Invalid input Try Again.")
+
+
+def Write_Mode():
+    # Taking Inputs
+    print("Please enter following details: ")
+    work_done = input("Work Done: ")
+    thoughts_feelings = input("Thoughts/Feelings: ")
+    learning = input("Learning: ")
+    # Appending data to file
+    with open(path, 'a') as journal:
+        journal.write(f"\n\n----#---- {todays_date} ----#----")
+        journal.write(
+            f"\nWork Done: {work_done}\nThoughts/Feelings: {thoughts_feelings}\nLearning: {learning}")
+        journal.write("\n-- ---- ---- ---- ---- ---- --")
+    print("Data added Successfully")
+
+
+def Read_Mode():
+    # Opening file in read mode
+    print("Here is your file Content:\n")
+    try:
+        with open(path) as journal:
+            content = journal.read()
+            print(content)
+    except FileNotFoundError:
+        print("No journal file found yet. Write something first!")
+
+
+def Footer():
+    print("\n               Stay consistent!")
+    print("                    -ART\n")
+
+
+# User Template:
+print("\n~~~ Welcome to Daily Journal ~~~")
+Mode_Selection()
+Footer()
